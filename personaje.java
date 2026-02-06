a5a70148aef3585631340a52bca63cfa3f44cb74
ManelLlurda <manelllurda@alumnes.ilerna.com>
2026-02-06 16:12:45 +0100

    clase personaje

---
+++ personaje.java
@@ -0,0 +1,67 @@
+
+public class personaje {
+	private String nombre = "";
+	private int nivel = 0;
+	private int puntosVida = 0;
+	private boolean protegido = false;
+	private inventario inventario;
+	
+	public personaje(String nombre, int nivel, int puntosVida, boolean protegido) {
+		this.nombre = nombre;
+		this.nivel = nivel;
+		this.puntosVida = puntosVida;
+		this.protegido = protegido;
+	}
+	
+
+	public void mostrarInfo(String nombre, int nivel, int puntosVida) {
+		System.out.println("Su nombre es " + nombre + " su nivel es " + nivel + " sus puntos de vida son " + puntosVida);
+	}
+
+	public void bajarVida(int cantidad) {
+		if (cantidad > 0) {
+			if (this.protegido==true) {
+			puntosVida = puntosVida-cantidad/2;
+			}
+			else puntosVida = puntosVida-cantidad;
+		}
+		else
+			System.out.println("Cantidad incorrrecta");
+	}
+	public void curar(int cantidad) {
+		if (cantidad > 0) puntosVida = puntosVida+cantidad;
+		else System.out.println("Cantidad incorrrecta");
+	}
+	public inventario getInventario() {
+		return inventario;
+	}
+	public void agragarEquipamiento(equipamiento equipamiento) {
+		inventario.add(equipamiento);
+	}
+	public void setProtegido(boolean protegido) {
+		if (protegido==true) this.protegido=true;
+		else this.protegido=false;
+	}
+	public boolean estaProtegido() {
+		return this.protegido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public int getNivel() {
+		return nivel;
+	}
+	public  void setNivel(int nivel) {
+		this.nivel=nivel;
+	}
+	public int getPuntosVida() {
+		return puntosVida;
+	}
+	public void setPuntosVida(int puntosVida) {
+		this.puntosVida=puntosVida;
+	}
+	
+}
